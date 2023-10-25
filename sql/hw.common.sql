a6216e3acb4873dde04b9a157211c4b4349be658
workHwang <138659912+workHwang@users.noreply.github.com>
2023-10-25 15:32:41 +0900

    Update hw.common.sql

--- sql/hw.common.sql
+++ sql/hw.common.sql
@@ -14,3 +14,17 @@ FROM (
     FROM DUAL) 
 CONNECT BY LEVEL < (SYSDATE-ST_DT) + 1 
 ORDER BY ST_DT+(LEVEL-1) DESC
+
+/*
+ * 현재일로부터 입력한 값에 해당하는 주차는 언제인지?
+ * @param:inputWeeks (음수: 과거 주차, 양수: 미래 주차)
+ */
+SELECT 
+      CEIL(LEVEL/7)||'주차' 주차
+    , A
+    , A+(CASE WHEN :inputWeeks >= 0 THEN LEVEL-1 ELSE -(LEVEL-1) END) B
+    , CASE WHEN A+(CASE WHEN :inputWeeks >= 0 THEN LEVEL-1 ELSE -(LEVEL-1) END) = TO_CHAR(SYSDATE, 'YYYYMMDD') THEN 'V' ELSE '' END 입력일
+FROM (SELECT TRUNC(TO_DATE(TO_CHAR(SYSDATE, 'YYYYMMDD')), 'D') A FROM DUAL)
+WHERE :inputWeeks <> 0 -- optional
+CONNECT BY LEVEL < ABS(:inputWeeks)*7 +1
+ORDER BY B DESC
