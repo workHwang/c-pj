5f51b3f5b0e3bbad07e97f1bd48e13da2d11fb00
workHwang <138659912+workHwang@users.noreply.github.com>
2023-10-25 14:29:43 +0900

    Update hw.common.sql

--- sql/hw.common.sql
+++ sql/hw.common.sql
@@ -6,12 +6,11 @@
 SELECT 
       CEIL(LEVEL/7) || '주차' 주차
     , ST_DT+(LEVEL-1) 일자
+    , CASE WHEN ST_DT+(LEVEL-1) = :inputDtm THEN 'V' ELSE '' END 입력일
     , ST_DT 주초일
-    , IPT_DT 입력일 
 FROM (
     SELECT 
           TRUNC(TO_DATE(:inputDtm, 'YYYYMMDD'), 'D') ST_DT
-        , TO_DATE(:inputDtm, 'YYYYMMDD') IPT_DT
     FROM DUAL) 
 CONNECT BY LEVEL < (SYSDATE-ST_DT) + 1 
 ORDER BY ST_DT+(LEVEL-1) DESC
